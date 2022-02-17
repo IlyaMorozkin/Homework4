4f5c178fcfd9f4d97345426aa9fabdd6019e7653
Илья Морозкин <morozkin.ilya86@mail.ru>
2022-02-17 07:40:39 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,10 @@
+﻿// Возведите число А в натуральную степень B, используя цикл.
+
+Console.WriteLine("Возвести число А в натуральную степень В");
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(a, b);
+Console.WriteLine($"{a} в степени {b} = {result}");
