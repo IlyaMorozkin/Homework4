c7b0d57e5fbb2dfda2c338d56b191f8062df8590
Илья Морозкин <morozkin.ilya86@mail.ru>
2022-02-17 08:06:48 +0300

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+int[] array = new int [8];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,2);
+
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+FillArray(array);
+PrintArray(array);
